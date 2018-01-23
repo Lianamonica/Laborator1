fa197d4f2ecd7408f7e7e6b16ff3e3e435b22e66
Monica Rad <33666310+Lianamonica@users.noreply.github.com>
2018-01-24 00:14:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,8 @@
 
 using namespace std;
 
-//programul stabileste daca doua numere intregi introduse de la tastatura sunt in ordine crescatoare
-
 int main()
 {
-    int a,b;
-    cout << "Introduceti primul numar: ";
-    cin >> a;
-    cout << "Introduceti al doilea numar: ";
-    cin >> b;
-    if (a < b)
-        cout << "Numerele sunt in ordine crescatoare." << endl;
-    else
-        if (a > b)
-        cout << "Numerele sunt in ordine descrescatoare." << endl;
-        else
-            cout << "Numerele sunt egale." << endl;
+    cout << "Hello world!" << endl;
     return 0;
 }
